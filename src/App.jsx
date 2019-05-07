9f8c2413a984970267bb6df567df608acf2923d3
tw5033 <tw5033@gmail.com>
2019-05-07 00:40:06 +0000

    Create react components to render basic chatty page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
-  }
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/" className="navbar-brand">Chatty</a>
+    </nav>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <MessageList/>
+      <ChatBar/>
+    </div>   
+  )
 }
 export default App;
---
+++ src/Message.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+const Message = ({message}) => {
+  return message.type === "incomingMessage" ? (
+    <div className="message">
+      <span className="message-username">{message.username}</span>
+      <span className="message-content">{message.content}</span>
+    </div>
+  ) : (
+    <div class="message system">
+      {message.content}
+    </div>
+  )
+}
+
+export default Message;
