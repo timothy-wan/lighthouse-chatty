1bb4081c46e403a796c1daf536a92f055f8badc3
tw5033 <tw5033@gmail.com>
2019-05-08 18:46:38 +0000

    Separate Navbar Component into its own file

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,7 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
-
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <a href="/" className="navbar-brand">Chatty</a>
-    </nav>
-  )
-}
+import Navbar from './Navbar.jsx';
 
 class App extends Component {
   constructor(props) {
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/" className="navbar-brand">Chatty</a>
+    </nav>
+  )
+}
+
+export default Navbar;
